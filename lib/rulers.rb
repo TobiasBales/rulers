171bd10e28e6f54a9deafabb767247cd960279ec
Tobias Bales <git@tobiasbales.dev>
2022-10-13 22:30:01 +0200

    Chapter 2

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -3,11 +3,41 @@
 require "rulers/version"
 require "rulers/array"
 require "rulers/object"
+require "rulers/routing"
 
 module Rulers
   class Application
     def call(env)
-      [200, {'content-type' => 'text/html'}, ["Hello from Ruby on Rulers!"]]
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {'content-type' => 'text/html'}, []]
+      end
+
+      begin
+        klass, action = get_controller_and_action(env)
+      rescue NameError => e
+        return [404, {'content-type' => 'text/html'}, ["There is no controller named \"#{klass&.capitalize}Controller\""]]
+      end
+      controller = klass.new(env)
+
+      response = controller.send(action)
+
+      [200, {'content-type' => 'text/html'}, [response]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    attr_reader :env
+
+    def get?
+      env["REQUEST_METHOD"] == "GET"
+    end
+
+    def post?
+      env["REQUEST_METHOD"] == "POST"
     end
   end
 end
