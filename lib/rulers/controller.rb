fb1c363b46d3fdbc3faee43d20c5275ed8b19aa1
Tobias Bales <git@tobiasbales.dev>
2022-10-23 19:06:02 +0200

    Inject instance variables into templates

--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -7,6 +7,8 @@ module Rulers
   class Controller
     extend T::Sig
 
+    Locals = T.type_alias { T::Hash[String, T.untyped] }
+
     sig { params(env: Rulers::Env).void }
     def initialize(env)
       @env = env
@@ -15,13 +17,13 @@ module Rulers
     sig { returns(Env) }
     attr_reader :env
 
-    sig { params(view_name: Symbol, locals: T::Hash[Symbol, T.untyped]).returns(String) }
+    sig { params(view_name: Symbol, locals: Locals).returns(String) }
     def render(view_name, locals = {})
       filename = File.join("app", "views", controller_name, "#{view_name}.html.erb")
       template = File.read(filename)
       eruby = Erubis::Eruby.new(template)
 
-      eruby.result(locals.merge(env: env))
+      eruby.result(supplement_locals(locals))
     end
 
     sig { returns(String) }
@@ -37,6 +39,19 @@ module Rulers
     sig { returns(T::Boolean) }
     def post?
       env["REQUEST_METHOD"] == "POST"
+    end
+
+    private
+
+    sig { params(locals: Locals).returns(Locals) }
+    def supplement_locals(locals)
+      locals = locals.merge(controller_name: controller_name)
+
+      instance_variables.each do |instance_variable|
+        locals[instance_variable] = instance_variable_get(instance_variable)
+      end
+
+      locals
     end
   end
 end
